99e96925bf8e6eadc5ea171b2058277ccf31cab1
Chi's Macbook pro <chi.lee@almagesttech.com>
2017-08-11 10:48:01 +0800

    return slick

--- slick/slick.js
+++ slick/slick.js
@@ -2982,21 +2982,6 @@
 
     };
 
-    $.fn.slick = function() {
-        var _ = this,
-            opt = arguments[0],
-            args = Array.prototype.slice.call(arguments, 1),
-            l = _.length,
-            i,
-            ret;
-        for (i = 0; i < l; i++) {
-            if (typeof opt == 'object' || typeof opt == 'undefined')
-                _[i].slick = new Slick(_[i], opt);
-            else
-                ret = _[i].slick[opt].apply(_[i].slick, args);
-            if (typeof ret != 'undefined') return ret;
-        }
-        return _;
-    };
+    return Slick;
 
 }));
